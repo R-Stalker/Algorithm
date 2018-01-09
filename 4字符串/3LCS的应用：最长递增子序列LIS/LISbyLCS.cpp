85f7fb86e1748fe5f15e1a305a53ed765908007b
SilencerRONG <34083975+SilencerRONG@users.noreply.github.com>
2018-01-09 17:46:28 +0800

    .

--- 4字符串/3LCS的应用：最长递增子序列LIS/LISbyLCS.cpp
+++ 4字符串/3LCS的应用：最长递增子序列LIS/LISbyLCS.cpp
@@ -9,12 +9,11 @@ using std::endl;
 using std::string;
 using std::vector;
 using std::max;
+using std::sort;
 
-void LCS(const char* str1, const char* str2, string &str){
-	int size1 = (int)strlen(str1);
-	int size2 = (int)strlen(str2);
-	const char* s1 = str1 - 1;//从1开始数，方便代码编写
-	const char* s2 = str2 - 1;
+void LCS(string &str1, string &str2, string &str){
+	int size1 = str1.length();
+	int size2 = str2.length();
 	vector<vector<int> > chess(size1+1, vector<int>(size2+1));
 	for(int i = 0; i <= size1; i++){ //第0列
 		chess[i][0] = 0;
@@ -25,7 +24,7 @@ void LCS(const char* str1, const char* str2, string &str){
 
 	for(int i = 1; i <= size1; i++){
 		for(int j = 1; j <= size2; j++){
-			if(s1[i] == s2[j]){
+			if(str1[i-1] == str2[j-1]){
 				chess[i][j] = chess[i-1][j-1] + 1;
 			}else{
 				chess[i][j] = max(chess[i][j-1], chess[i-1][j]);
@@ -36,8 +35,8 @@ void LCS(const char* str1, const char* str2, string &str){
 	int i = size1;
 	int j = size2;
 	while( (i != 0) && (j != 0)){
-		if(s1[i] == s2[j]){
-			str.push_back(s1[i]);
+		if(str1[i-1] == str2[j-1]){
+			str.push_back(str1[i-1]);
 			i--;
 			j--;
 		}else{
@@ -52,8 +51,9 @@ void LCS(const char* str1, const char* str2, string &str){
 }
 
 int main(){
-	const char* str1 = "ABCBDAB";
-	const char* str2 = "BDCABA";
+	string str1 = "567128";
+	string str2 = str1;
+	sort(str2.begin(), str2.end());
 	string str;
 	LCS(str1, str2, str);
 	cout << str.c_str() << endl;    //LCS可能不唯一
